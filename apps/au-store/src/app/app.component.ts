1a1ffa60952211348f9d39ebdb32258efdf1cca1
Curchod <timothy.curchod@accenture.com>
2021-07-16 11:48:11 +1000

    added au-store app

---
+++ apps/au-store/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 't7-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'au-store';
+}
